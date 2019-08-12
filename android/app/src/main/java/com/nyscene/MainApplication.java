97dac4426a49cc167c26431e329ab4cec17507cd
Ricardo Pineda <richay718@gmail.com>
2019-08-12 00:03:28 -0400

    added dependencies and configured android files

--- android/app/src/main/java/com/nyscene/MainApplication.java
+++ android/app/src/main/java/com/nyscene/MainApplication.java
@@ -10,6 +10,7 @@ import com.facebook.react.ReactApplication;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.airbnb.android.react.maps.MapsPackage;
 
 import java.util.List;
 
